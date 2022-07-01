4cf2dc079bcb917c9db5ad2355d614585b692629
Shashank Maurya <shashankmaurya260101@gmail.com>
2022-07-02 04:52:41 +0530

    Video Support added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 import 'model/user.dart';
 
@@ -16,20 +17,19 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-
   // List<User> users=getUsers();
   late Future<List<User>> usersFuture;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
-    usersFuture=getUsers(context);
+    usersFuture = getUsers(context);
   }
 
   static Future<List<User>> getUsers(BuildContext context) async {
-    final assetBundle=DefaultAssetBundle.of(context);
-    final data=await assetBundle.loadString('lib/assets/dataset.json');
+    final assetBundle = DefaultAssetBundle.of(context);
+    final data = await assetBundle.loadString('lib/assets/dataset.json');
     // const data=[
     //   {
     //     "id": 1,
@@ -44,17 +44,14 @@ class _MainPageState extends State<MainPage> {
     //     "coverPicture": "https://picsum.photos/800/450"
     //   }
     // ];
-    final body=json.decode(data);
+    final body = json.decode(data);
     return body.map<User>(User.fromJson).toList();
   }
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          primarySwatch: Colors.red
-      ),
+      theme: ThemeData(primarySwatch: Colors.red),
       home: Scaffold(
         appBar: AppBar(
           // backgroundColor: const Color(0xff885566),
@@ -62,51 +59,67 @@ class _MainPageState extends State<MainPage> {
           centerTitle: true,
         ),
         body: Center(
-          // child: buildUsers(users)
+            // child: buildUsers(users)
             child: FutureBuilder<List<User>>(
-              future: usersFuture,
-              builder: (context, snapshot){
-                if(snapshot.hasData){
-                  final users=  snapshot.data!;
-                  return buildUsers(users);
-                }
-                else {
-                  return const Text('no videos available');
-                }
-              },
-            )
-        ),
+          future: usersFuture,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final users = snapshot.data!;
+              return buildUsers(users);
+            } else {
+              return const Text('no videos available');
+            }
+          },
+        )),
       ),
     );
   }
 
   Widget buildUsers(List<User> users) => ListView.builder(
-    itemCount: users.length,
-    itemBuilder: (context, index) {
-      final user = users[index];
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
 
-      return Card(
-        child: ListTile(
-          leading: CircleAvatar(
-            radius: 28,
-            backgroundImage: NetworkImage(user.coverPicture),
-          ),
-          title: Text(user.title),
-        ),
+          // return Card(
+          //   child: ListTile(
+          //     leading: CircleAvatar(
+          //       radius: 28,
+          //       backgroundImage: NetworkImage(user.coverPicture),
+          //     ),
+          //     title: Text(user.title),
+          //   ),
+          // );
+
+          return Card(
+              child: Column(children: <Widget>[
+            Column(children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  radius: 28,
+                  backgroundImage: NetworkImage(user.coverPicture),
+                ),
+                title: Text(user.title),
+              ),
+              Stack(
+                  alignment: FractionalOffset.bottomRight +
+                      const FractionalOffset(-0.1, -0.1),
+                  children: <Widget>[
+                    videoply(vrl: user.videoUrl),
+                  ]),
+            ]),
+          ]));
+        },
       );
-    },
-  );
 
-  // @override
-  // Widget build(BuildContext context) => Scaffold(
-  //   appBar: AppBar(
-  //       title: const Text('Youtube'),
-  //       centerTitle: true,
-  //   ),
-  // );
+// @override
+// Widget build(BuildContext context) => Scaffold(
+//   appBar: AppBar(
+//       title: const Text('Youtube'),
+//       centerTitle: true,
+//   ),
+// );
 
 }
-
 
 // class MyApp extends StatelessWidget {
 //   const MyApp({Key? key}) : super(key: key);
@@ -131,8 +144,6 @@ class _MainPageState extends State<MainPage> {
 //   return data.map<User>(User.fromJson).toList();
 // }
 
-
-
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
@@ -148,3 +159,69 @@ class _MainPageState extends State<MainPage> {
 //     );
 //   }
 // }
+
+class videoply extends StatefulWidget {
+  final String vrl;
+
+  const videoply({Key? key, required this.vrl}) : super(key: key);
+
+  @override
+  State<videoply> createState() => _videoplyState(vrl);
+}
+
+class _videoplyState extends State<videoply> {
+  // late String vurl;
+  late VideoPlayerController _controller;
+  late Uri uri;
+
+  _videoplyState(String s) {
+    uri = Uri.parse(s);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.contentUri(uri);
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.setLooping(true);
+    _controller.initialize().then((_) => setState(() {}));
+    _controller.play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(top: 20.0),
+          ),
+          const Text('With assets mp4'),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  VideoPlayer(_controller),
+                  // _ControlsOverlay(controller: _controller),
+                  VideoProgressIndicator(_controller, allowScrubbing: true),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
